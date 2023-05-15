b1be731edd348c4482f47b58d88b3299e432891c
yordanraychev <131948873+yordanraychev@users.noreply.github.com>
2023-05-15 22:02:31 +0300

    Added exercises

---
+++ 01_programming_basics_with_python/01_first_steps_in_coding_more_exercises/01_trapeziod_area.py
@@ -0,0 +1,7 @@
+base_a = float(input())
+base_b = float(input())
+height = float(input())
+
+area = (base_a + base_b) * height / 2
+
+print(f"{area:.2f}")
---
+++ 01_programming_basics_with_python/01_first_steps_in_coding_more_exercises/02_triangle_area.py
@@ -0,0 +1,6 @@
+base = float(input())
+height = float(input())
+
+area = base * height / 2
+
+print(f"{area:.2f}")
