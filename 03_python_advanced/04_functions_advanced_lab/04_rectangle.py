322f9d4ce344a5adca84bbd6e85e5245908121a0
yordanraychev <yordan.raych@gmail.com>
2024-01-27 22:51:57 +0200

    Added exercises

---
+++ 03_python_advanced/04_functions_advanced_lab/04_rectangle.py
@@ -0,0 +1,11 @@
+def rectangle(length, width):
+    if type(length) is not int or type(width) is not int:
+        return "Enter valid values!"
+
+    def area():
+        return length * width
+
+    def perimeter():
+        return length * 2 + width * 2
+
+    return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
