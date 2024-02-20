865912d2adae81ef715cdc28a3b36834824545ee
yordanraychev <yordan.raych@gmail.com>
2024-02-21 00:02:25 +0200

    Added exercises

---
+++ 03_python_advanced/09_algorithms_introduction/07_selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(nums):
+    for idx in range(len(nums)):
+        min_idx = idx
+
+        for current_idx in range(idx + 1, len(nums)):
+            if nums[current_idx] < nums[min_idx]:
+                min_idx = current_idx
+
+        nums[idx], nums[min_idx] = nums[min_idx], nums[idx]
+
+
+nums = [int(x) for x in input().split()]
+selection_sort(nums)
+print(*nums)
