4285454e6389b576f37fd71fcc8fc63ce6e75f9c
yordanraychev <yordan.raych@gmail.com>
2024-03-24 17:12:48 +0200

    Added exercises

---
+++ 04_python_oop/06_polymorphism_and_abstraction_lab/03_playing.py
@@ -0,0 +1,20 @@
+def start_playing(obj):
+    return obj.play()
+
+
+class Guitar:
+    @staticmethod
+    def play():
+        return "Playing the guitar"
+
+
+class Children:
+    @staticmethod
+    def play():
+        return "Children are playing"
+
+
+# children = Children()
+# print(start_playing(children))
+# guitar = Guitar()
+# print(start_playing(guitar))
